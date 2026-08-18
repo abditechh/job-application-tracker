970841849cad2d1dc578b1a33a1f8a7203758bc5
abditechh <abdi.abdirashid@hotmail.com>
2026-08-18 23:50:06 +0200

    Add firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase web configuration is public client-side information.
+// Access to user records must be protected by the included Firestore rules.
+export const firebaseConfig = {
+  apiKey: "AIzaSyDSxSTUlORucGxkInmgMLtVDLKUmqi8apU",
+  authDomain: "abdi-926ed.firebaseapp.com",
+  projectId: "abdi-926ed",
+  storageBucket: "abdi-926ed.firebasestorage.app",
+  messagingSenderId: "841042032568",
+  appId: "1:841042032568:web:2ded04713465e77aac814d",
+  measurementId: "G-6QFEXE38M5"
+};
